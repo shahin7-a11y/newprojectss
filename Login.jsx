85827823004e8c246574124ff0018912d4659667
shahin7-a11y <shahinfawas@gmail.com>
2025-09-09 15:10:44 +0530

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,14 @@
+import Homebg from "../../../components/homepagebg/Homebg"
+import Navbar from "../../../components/Nav/Nav"
+
+function Login(){
+    return(
+        <div>
+            <Navbar/>
+            
+            <Homebg/>
+           
+        </div>
+    )
+}
+export{Login}
